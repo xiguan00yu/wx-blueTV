f068f11b0ea4405f11f009cb79f6141f5f2856eb
linkerlam <linkerlam@httpeace.com>
2020-11-18 23:26:58 +0800

    change ui and createConnect

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,9 @@ const app = getApp()
 Page({
   data: {
     scaning: false,
-    devices: []
+    devices: [],
+    connectBle: {},
+    connectDevice: null
   },
   //事件处理函数
   toLogScreen: function () {
@@ -34,8 +36,70 @@ Page({
     }
   },
   onConnectBlueItem: async function (event) {
-    const deviceId = event.currentTarget.dataset.id
-    console.log('deviceId', deviceId)
+    wx.showLoading({
+      title: 'connecting ...'
+    })
+    await (async () => {
+      const deviceId = event.currentTarget.dataset.id
+      console.log('click ble item', deviceId)
+      if (this.data.connectBle.deviceId) {
+        // need close Connection
+        const closeConnectRes = await util.wxAsyncPromise('closeBLEConnection', {
+          deviceId
+        })
+        if (closeConnectRes._fail) return
+        this.setData({
+          connectBle: {},
+          connectDevice: null
+        })
+        console.log('close connect ble ', closeConnectRes.errMsg)
+        return
+      }
+      // ===== into connect =====
+      const createBle = await util.wxAsyncPromise('createBLEConnection', {
+        deviceId
+      });
+      if (createBle._fail) return
+      console.log('create ble connect', deviceId)
+      const bleServices = await util.wxAsyncPromise('getBLEDeviceServices', {
+        deviceId
+      });
+      if (bleServices._fail) return
+      console.log('connect blue get services', bleServices.services)
+      const needCheckServices = bleServices.services || []
+      // record connect
+      const connectBle = {}
+      for (let i = 0; i < needCheckServices.length; i++) {
+        const service = needCheckServices[i];
+        const bleCharacter = await util.wxAsyncPromise('getBLEDeviceCharacteristics', {
+          deviceId,
+          serviceId: service.uuid
+        });
+        console.log('get charateristics', bleCharacter.errMsg)
+        if (bleCharacter._fail) continue
+        const findCharacter = bleCharacter.characteristics.find(c => c.properties.write)
+        console.log('connect blue get character', findCharacter)
+        if (findCharacter) {
+          Object.assign(connectBle, {
+            deviceId,
+            serviceId: service.uuid,
+            characteristicId: findCharacter.uuid,
+          })
+          break;
+        }
+      }
+      if (Object.keys(connectBle).length > 0) {
+        this.setData({
+          connectBle,
+          connectDevice: this.data.devices.find(d => d.deviceId === deviceId)
+        })
+      }
+
+
+      this.stopScan()
+
+    })()
+    wx.hideLoading()
   },
   onLoad: async function () {
     const that = this
@@ -52,5 +116,12 @@ Page({
     const initBlue = await util.wxAsyncPromise('openBluetoothAdapter')
     console.log('initBlue', initBlue.errMsg)
     if (initBlue._fail) return
-  }
+  },
+  onTryScan: function () {
+    if (!this.data.scaning) this.startScan()
+    if (this.data.scaning) this.stopScan()
+  },
+  onHide: function () {
+    if (this.data.scaning) this.stopScan()
+  },
 })
